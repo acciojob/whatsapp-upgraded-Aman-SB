6f97247d5d395996b48ae0cbd9f459663e10f258
Aman Bisht <bishtaman60@gmail.com>
2023-07-22 16:36:57 +0530

    Solution 1.0

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Group {
     private String name;
     private int numberOfParticipants;
--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -2,6 +2,14 @@ package com.driver;
 
 import java.util.Date;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Message {
     private int id;
     private String content;
